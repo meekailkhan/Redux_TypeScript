6afcad2650437fae4ce05884da21cc39bdd07e12
pc <meekailkhan123aar@gmail.com>
2025-03-23 15:58:45 +0530

    react typescript redux

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { Provider} from 'react-redux'
 import store from './app/store.js'
 import App from './App.jsx'
 
-const rootElement = document.getElementById('root');
+// const rootElement = document.getElementById('root');
 
-if (!rootElement) throw new Error("Root element not found");
+// if (!rootElement) throw new Error("Root element not found");
 
-createRoot(rootElement).render(
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   
   <StrictMode>
   <Provider store={store}>
