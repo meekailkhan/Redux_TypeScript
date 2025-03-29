8346213c87e6dd100e606e69815aefc0bc868d8c
pc <meekailkhan123aar@gmail.com>
2025-03-29 17:16:07 +0530

    react-typescript-redux

--- src/features/cake/CakeView.tsx
+++ src/features/cake/CakeView.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 // import { useSelector,useDispatch } from 'react-redux'
 import { useAppSelector,useAppDispatch } from '../../app/hooks'
 import { ordered,restored } from './cakeSlice'
